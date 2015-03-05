c849029449d7bc08527619f1d63c6f3e047453fd
Jacob Turner <j.israel.turner@gmail.com>
2015-03-04 22:00:00 -0700

    :boar:

--- public/js/controller.js
+++ public/js/controller.js
@@ -6,14 +6,6 @@ app.controller('MainController', function($scope, mainService) {
 
 	$scope.insertFormula = function(item){
 		console.log(item);
-
-		Office.context.document.bindings.addFromNamedItemAsync('A1:E' + spreadsheetData.length, 'matrix', {id:'invoiceLineItems'}, function (asyncResult) {
-	    if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-	      // add the data
-	      Office.select('bindings#invoiceLineItems').setDataAsync(spreadsheetData, {coercionType:'matrix'}, function(asyncResult){
-	      });
-	    }
-	  })
 	}
 
 });
