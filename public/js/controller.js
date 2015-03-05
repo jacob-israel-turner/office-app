c26cb0a17734fa13a7b2bc69f1a81cdb24db0b51
Jacob Turner <j.israel.turner@gmail.com>
2015-03-04 21:46:21 -0700

    Creating basic functionality. Filter, ng-repeat, etc.

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,5 +1,11 @@
 var app = angular.module('cheet');
 
-app.controller('MainController', function($scope) {
-  $scope.test = "welcome";
+app.controller('MainController', function($scope, mainService) {
+
+	$scope.cheats = mainService.getCheats();
+
+	$scope.insertFormula = function(item){
+		console.log(item);
+	}
+
 });
--- public/js/service.js
+++ public/js/service.js
@@ -2,4 +2,19 @@ var app = angular.module('cheet');
 
 app.service('mainService', function() {
 
+	var cheats = [
+		{
+			name: 'add',
+			text: 'A1:B1'
+		},
+		{
+			name: 'subtract',
+			text: 'somethingELSE!'
+		}
+	]
+
+	this.getCheats = function(){
+		return cheats;
+	}
+
 });
